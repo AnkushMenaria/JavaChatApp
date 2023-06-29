7d19a246c4b9604dbf87d6ff2a70ddd8656b18c6
AnkushMenaria <136432457+AnkushMenaria@users.noreply.github.com>
2023-06-29 11:03:55 +0300

    Update ChatServer.java

--- ChatApp/ChatServer.java
+++ ChatApp/ChatServer.java
@@ -4,15 +4,19 @@ import java.net.*;
 public class ChatServer {
     public static void main(String[] args) {
         try {
-            ServerSocket serverSocket = new ServerSocket(12345); // Change the port number if needed
+            ServerSocket serverSocket = new ServerSocket(12345); 
             System.out.println("Server started. Listening on port 12345...");
 
             Socket clientSocket = serverSocket.accept();
             System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
 
+            // Input stream for receiving messages from the client
             BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            
+            // Output stream for sending messages to the client
             PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
 
+            // Input stream for reading messages from the console
             BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
 
             String inputLine;
@@ -24,6 +28,7 @@ public class ChatServer {
                 writer.println(response);
             }
 
+            // Close streams and sockets
             reader.close();
             writer.close();
             consoleReader.close();
